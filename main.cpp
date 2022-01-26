3d15ea79ebc314980e9fd0350d396e3dde9d7ab7
nikhilgehi <nikhilgehi15@gmail.com>
2022-01-26 12:05:56 +0530

    first commit

--- main.cpp
+++ main.cpp
@@ -1,24 +1,73 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
-  bool palindrome(string s,int n)
+// Input: nums = [3,1,-2,-5,2,-4]
+// Output: [3,-2,1,-5,2,-4]
+vector<int> rearrangeArray(vector<int> &nums)
+{
+    vector<int> ans;
+    int n = nums.size();
+    if (n == 0)
     {
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]!=s[n-i-1])
-            {
-                return false;
-                break;
-            }
-        }
-        return true;
+        return ans;
     }
+    vector<int> positive;
+    vector<int> negative;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] > 0)
+        {
+            positive.push_back(nums[i]);
+        }
+        else
+            negative.push_back(nums[i]);
+    }
+    ans.clear();
+    for (int i = 0; i < positive.size(); i++)
+    {
+        ans.push_back(positive[i]);
+        ans.push_back(negative[i]);
+    }
+    return ans;
+}
+vector<int> findLonely(vector<int> &nums)
+{
+    vector<int> ans;
+    int n = nums.size();
+    if (n == 0)
+    {
+        return ans;
+    }
+    map<int,int>m;
+    for (int i = 0; i < n; i++)
+    {
+        m[nums[i]]++;
+    }
+    for(pair<int,int>n:m)
+    {
+        int x=n.first,y=n.second;
+        if (y==1 && mp.count(x+1)==0 && mp.count(x-1)==0)
+        {
+            ans.push_back(x);
+        }
+        
+    }
+    return ans;
+    
+        
+    
+}
 int main()
 {
-int n;
-cin>>n;
-string s;
-cin>>s;
-cout<<palindrome(s,n);
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+
+    }
+
     return 0;
 }
